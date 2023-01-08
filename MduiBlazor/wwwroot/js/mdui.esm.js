d58e4132723e83ad66390707eedf7df46f96b05a
gaofen13 <gaofen13@163.com>
2023-01-08 19:03:10 +0800

    update lib

--- MduiBlazor/wwwroot/js/mdui.esm.js
+++ MduiBlazor/wwwroot/js/mdui.esm.js
@@ -260,9 +260,6 @@ function get$() {
 }
 const $ = get$();
 
-// 避免页面加载完后直接执行css动画
-// https://css-tricks.com/transitions-only-after-page-load/
-setTimeout(() => $('body').addClass('mdui-loaded'));
 const mdui = {
     $: $,
 };
