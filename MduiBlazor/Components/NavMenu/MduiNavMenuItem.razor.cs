b4d0c73525dc743b75e3d9492d42fb923c05891d
gaofen13 <gaofen13@163.com>
2023-01-29 10:06:34 +0800

    修复menu ripple效果

--- MduiBlazor/Components/NavMenu/MduiNavMenuItem.razor.cs
+++ MduiBlazor/Components/NavMenu/MduiNavMenuItem.razor.cs
@@ -7,7 +7,8 @@ namespace MduiBlazor
     public partial class MduiNavMenuItem : MduiComponentBase
     {
         protected string Classname =>
-            new ClassBuilder("mdui-list-item mdui-ripple")
+            new ClassBuilder("mdui-list-item")
+            .AddClass("mdui-ripple", Ripple)
             .AddClass("mdui-typo", UseMduiTypo)
             .AddClass(Class)
             .Build();
@@ -17,6 +18,9 @@ namespace MduiBlazor
 
         [Parameter]
         public string? Icon { get; set; }
+
+        [Parameter]
+        public bool Ripple { get; set; } = true;
 
         [Parameter]
         public NavLinkMatch Match { get; set; } = NavLinkMatch.Prefix;
