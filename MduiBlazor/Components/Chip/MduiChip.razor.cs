1e7ec6504db1e300e2dc5fcd2fbc2a58b0885b6f
gaofen13 <gaofen13@163.com>
2023-02-02 14:01:10 +0800

    add chip component

---
+++ MduiBlazor/Components/Chip/MduiChip.razor.cs
@@ -0,0 +1,40 @@
+using MduiBlazor.Utilities;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace MduiBlazor
+{
+    public partial class MduiChip : MduiComponentBase
+    {
+        protected string Classname =>
+            new ClassBuilder("mdui-chip")
+            .AddClass(Class)
+            .Build();
+
+        private string IconClassname =>
+            new ClassBuilder("mdui-chip-icon")
+            .AddClass($"mdui-color-{IconColor}", string.IsNullOrWhiteSpace(Image) && !string.IsNullOrWhiteSpace(Icon))
+            .Build();
+
+        [Parameter, EditorRequired]
+        public string? Title { get; set; }
+
+        [Parameter]
+        public string? Image { get; set; }
+
+        [Parameter]
+        public string? Icon { get; set; }
+
+        [Parameter]
+        public string? IconColor { get; set; }
+
+        [Parameter]
+        public string? IconLetter { get; set; }
+
+        [Parameter]
+        public bool DeleteButton { get; set; }
+
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnDeleteButtonClicked { get; set; }
+    }
+}
