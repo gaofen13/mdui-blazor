32d4b635c41d27f93a2583fb3568dd6193351b70
gaofen13 <gaofen13@163.com>
2023-02-15 13:54:16 +0800

    完善attribute

--- MduiBlazor/Components/Dialog/MduiDialog.razor.cs
+++ MduiBlazor/Components/Dialog/MduiDialog.razor.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using MduiBlazor.Utilities;
+using Microsoft.AspNetCore.Components;
 
 namespace MduiBlazor
 {
     public partial class MduiDialog : MduiComponentBase
     {
+        protected string Classname =>
+            new ClassBuilder("mdui-dialog-content")
+            .AddClass(Class)
+            .Build();
+
         [Parameter] public RenderFragment? ActionsContent { get; set; }
     }
 }
