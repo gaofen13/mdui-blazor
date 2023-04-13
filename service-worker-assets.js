2457ad6115f8b82a9c1be455e7c786488daa4712
gaofen13 <gaofen13@163.com>
2023-04-13 13:26:58 +0000

    Deploying to gh-pages from @ gaofen13/mdui-blazor@cff625f7be2d80d33276ad3809d550cfb2c21f18 🚀

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -33,7 +33,7 @@
       "url": "_framework\/icudt.dat"
     },
     {
-      "hash": "sha256-A1yo\/v5Kt+0KE6O3vDzkrSO0sluGm5wTE4YNKwRKpsg=",
+      "hash": "sha256-V0JghwqO0v3TkM7UcXjdETlErLaDOAIuS2dySznBftQ=",
       "url": "_framework\/blazor.boot.json"
     },
     {
@@ -41,7 +41,7 @@
       "url": "_framework\/MduiBlazor.Client.dll"
     },
     {
-      "hash": "sha256-ZBC6OQm6fhd5ggl6yznZ3Ya5p\/rSx1bGYEYhh4gyu1g=",
+      "hash": "sha256-+16QZNL7+DvU9gRiIwV4hJx9rzPecGKClnF3z\/XEEOw=",
       "url": "_framework\/MduiBlazor.dll"
     },
     {
@@ -377,5 +377,5 @@
       "url": "_content\/MduiBlazor\/icons\/material-icons\/MaterialIcons-Regular.woff2"
     }
   ],
-  "version": "rTxw9rto"
+  "version": "Zk6PqJsW"
 };
--- service-worker.js
+++ service-worker.js
@@ -46,4 +46,4 @@ async function onFetch(event) {
 
     return cachedResponse || fetch(event.request);
 }
-/* Manifest version: rTxw9rto */
+/* Manifest version: Zk6PqJsW */
