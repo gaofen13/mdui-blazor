058437b6c0936ca15ebadb53d3d80c7005ff1be4
gaofen13 <gaofen13@163.com>
2023-04-23 17:13:13 +0800

    update and fix

--- MduiBlazor.Server/Program.cs
+++ MduiBlazor.Server/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddHttpClient();
 
 builder.Services.AddMduiBlazor();
 
--- MduiBlazor.Shared/Pages/MaterialIcons.razor.cs
+++ MduiBlazor.Shared/Pages/MaterialIcons.razor.cs
@@ -1,18 +1,27 @@
 using MduiBlazor.Shared.Data;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
 using System.Text.Json;
 
 namespace MduiBlazor.Shared.Pages
 {
     public partial class MaterialIcons
     {
+        private bool _show;
+        private string? _selectedIcon;
         private IEnumerable<MaterialIcon> _showIcons = Enumerable.Empty<MaterialIcon>();
         private IEnumerable<MaterialIcon> _icons = Enumerable.Empty<MaterialIcon>();
 
-        protected override void OnInitialized()
-        {
-            var iconstring = File.ReadAllText("material_icon.json");
+        [Inject]
+        private HttpClient HttpClient { get; set; } = default!;
 
-            var icons = JsonSerializer.Deserialize<MaterialIcon[]>(iconstring, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+        [Inject]
+        private NavigationManager Navigation { get; set; } = default!;
+
+        protected override async Task OnInitializedAsync()
+        {
+            HttpClient.BaseAddress = new Uri(Navigation.BaseUri);
+            var icons = await HttpClient.GetFromJsonAsync<MaterialIcon[]>("_content/MduiBlazor.Shared/data/material_icon.json");
             if (icons != null)
             {
                 _icons = icons;
@@ -31,6 +40,12 @@ namespace MduiBlazor.Shared.Pages
                 _showIcons = _icons.Where(i => i.Name?.Contains(value.Trim()) == true);
             }
             StateHasChanged();
+        }
+
+        private void OnClickIconItem(string? name)
+        {
+            _show = true;
+            _selectedIcon = name;
         }
     }
 }
