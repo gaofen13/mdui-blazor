fee4c3ac369216da3f465d55874449c6497efad7
gaofen13 <gaofen13@163.com>
2025-11-24 09:13:57 +0800

    fix webapp rendermode-auto

--- MduiBlazor.Client/Program.cs
+++ MduiBlazor.Client/Program.cs
@@ -1,5 +1,13 @@
+using MduiBlazor;
+using MduiBlazor.Shared.Data;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<MaterialIconList>();
+
+builder.Services.AddMduiBlazor();
 
 await builder.Build().RunAsync();
